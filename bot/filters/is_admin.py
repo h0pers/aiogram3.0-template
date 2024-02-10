bcd1383319a9262360d69c5d2572af81c0e5b4b5
Dmytro Hryshchenko <zzzxxxxx28540@gmail.com>
2024-02-10 16:44:55 +0000

    Filter for admin handlers

---
+++ bot/filters/is_admin.py
@@ -0,0 +1,24 @@
+from aiogram.filters import BaseFilter
+from aiogram.types import Message, CallbackQuery
+
+from bot.config import ADMINS_ID
+
+
+class OnlyAdmin(BaseFilter):
+    async def __call__(self, message: Message, *args, **kwargs):
+        for admin_id in ADMINS_ID:
+            if message.from_user.id == admin_id:
+                print(message.from_user.id)
+                print(admin_id)
+                return True
+
+        return False
+
+
+class OnlyAdminCallback(BaseFilter):
+    async def __call__(self, query: CallbackQuery, *args, **kwargs):
+        for admin_id in ADMINS_ID:
+            if query.from_user.id == admin_id:
+                return True
+
+        return False
