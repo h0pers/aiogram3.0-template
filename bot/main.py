9c79797eb3a469889593c6e7725ad8b0efd970ac
h0pers <107421700+h0pers@users.noreply.github.com>
2024-07-29 14:59:42 +0100

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -14,7 +14,7 @@ dp = Dispatcher(storage=Settings.BOT.REDIS_STORAGE)
 async def start_bot():
     load_dotenv(dotenv_path=os.path.join(Settings.BASE_DIR, '.env'))
     dp.message.outer_middleware(CollectData())
-    dp.message.outer_middleware(CollectCallbackData())
+    dp.callback_query.outer_middleware(CollectCallbackData())
     dp.include_routers(*get_all_routers())
     bot = Bot(token=Settings.BOT.TOKEN, parse_mode=ParseMode.HTML)
     await dp.start_polling(bot)
